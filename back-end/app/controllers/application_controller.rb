0f895bb933b5cc9524b69436b1a268304d018090
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-03 15:02:21 +0300

    restaurants GET

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < Sinatra::Base 
+    set :default_content_type, 'application/json'
+
+    get '/restaurants' do
+        restaurants = Restaurant.all
+        restaurants.to_json
+    end
 end
